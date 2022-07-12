299f292bd661b78edf5a5fc078f2c0ba46cea81a
Durga Gadiraju <durga@skyhive.io>
2022-07-12 07:48:11 +0530

    Updated Content

--- mastering-emr-backup/02_getting_started/app.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession. \
-    builder. \
-    master('local'). \
-    appName('GitHub Activity - Getting Started'). \
-    getOrCreate()
-
-spark.sql('SELECT current_date').show()
